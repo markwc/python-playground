ef82ea8757906c1875c70ed112a0d9004877bb15
Christiansen, Mark W @ CSG - CSW <mark.w.christiansen@l-3com.com>
2015-11-17 21:25:02 -0700

    starting some simple stuff

---
+++ command-line.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+while True:
+    print('Enter command string: ')
+    command_string = input()
+    if '' == command_string:
+        break
+    arguments = command_string.split()
+    if ('test1' == arguments[0]):
+        for arg in arguments:
+            print(arg)
+    else:
+        print(arguments[0] + ': command not found')
+    
