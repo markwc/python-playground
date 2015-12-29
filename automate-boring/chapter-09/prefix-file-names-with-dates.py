e303a65ab5ecb74ec140a5218ef3ed371713dde7
Mark Christiansen <mark@markwc.com>
2015-12-29 07:22:02 -0700

    Create prefix-file-names-with-dates.py

---
+++ automate-boring/chapter-09/prefix-file-names-with-dates.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import datetime
+import os
+import re
+import shutil
+import sys
+
+def modificationDate(fileName):
+    timeStamp = os.path.getctime(fileName)
+    return datetime.datetime.fromtimestamp(timeStamp).strftime('%y%m%d%H%M%S')
+
+if __name__ == "__main__":
+    for oldFileName in sys.argv[1:]:
+        fileDate = modificationDate(oldFileName)
+        newFileName = fileDate + '-' + oldFileName
+        print (oldFileName + ' --> ' + newFileName)
+        shutil.move(oldFileName, newFileName)
